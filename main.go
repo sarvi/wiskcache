7ea08f5fb3f1f9cb19bd0bb1cfa2d3378c5f72f4
Li Du <ldu@cisco.com>
2021-03-10 11:59:58 -0800

    add sample calles to modules

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import(
+    "fmt"
+    "utils"
+    "cache"
+    "manifest"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+
+    if utils.Exists("go.mod"){
+        fmt.Println("Yes")
+    }else{
+        fmt.Println("No")
+    }
+    cache.Greet()
+    hash, err := manifest.GetHash("main.go")
+    if err != nil {
+        fmt.Println(err)
+    }else{
+        fmt.Println(hash)
+    }
 }
