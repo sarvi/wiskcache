6b1d636e4238fe7c06b9b677a9023e0c5744f1c9
Saravanan Shanmugham <sarvi@cisco.com>
2021-03-11 00:56:48 -0800

    Main core code in place to be filled out.

--- main.go
+++ main.go
@@ -11,6 +11,30 @@ import (
 )
 
 func main() {
+	// env := os.Environ()
+	// conf, cmd = argparser.ArgParse()
+	// cmdhash = whash.cmdhash(conf, env, cmd)
+	// man = cache.findmanifest(cmdhash)
+	// if config.mode == "active" || config.mode == "verify" {
+	// 	if man == nil {
+	// 		exitcode, infiles, outfiles = exec.runcmd(cmd)
+	// 		if exitcode != 0 {
+	// 			exit(exitcode)
+	// 		}
+	// 		man = manifest.new(man, infile, outfiles)
+	// 		cache.create(man)
+	// 		exit(0)
+	// 	} else {
+	// 		if config.mode == "active" {
+	// 			cache.copyout(man) // Copy out content from cache
+	// 		} else {
+	// 			cache.verify(man) // Verify content of Manifest with content of workspace
+	// 		}
+	// 		exit(0)
+	// 	}
+	// } else {
+	// 	manfiest.learn(conf, env, cmd, cmdhash)
+	// }
 
 	if utils.Exists("go.mod") {
 		fmt.Println("Yes")
