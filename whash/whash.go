f3e9bdd467bfbd6b71c00c640cd858c201aa9582
Saravanan Shanmugham <sarvi@cisco.com>
2021-03-10 23:56:52 -0800

    More test case whash normalize command before hashing

--- whash/whash.go
+++ whash/whash.go
@@ -3,23 +3,50 @@ package whash
 import (
 	"config"
 	"fmt"
+	"strings"
 
 	"lukechampine.com/blake3"
 )
 
-// func cmdnormalize(c config.Config, cmd []string) []string {
-// 	rv := []string
-// 	for _, p := range cmd {
-// 		p := strings.ReplaceAll(p, c.BaseDir, "./")
-// 		rv :=
-// 	}
-// }
+func pathtorel(path string, relativeto string) (rv string) {
+	rv = strings.Replace(path, relativeto, "", 1)
+	if rv != path {
+		for strings.Index(rv, "/") == 0 {
+			rv = strings.Replace(rv, "/", "", 1)
+		}
+		rv = strings.Join([]string{"./", rv}, "")
+	}
+	// fmt.Println("pathtorel: ", path, rv)
+	return
+}
+
+func cmdnormalize(c config.Config, cmd []string) (rv []string) {
+	// fmt.Println("BaseDir: ", c.BaseDir)
+	for _, p := range cmd {
+		// fmt.Println("replacing: ", p, c.BaseDir)
+		rv = append(rv, pathtorel(p, c.BaseDir))
+	}
+	// fmt.Println("Cmd: ", rv)
+	return
+}
+
+func envnormalize(c config.Config, env map[string]string) (rv map[string]string) {
+	rv = make(map[string]string)
+	for k, v := range env {
+		// fmt.Println("replacing: ", p, c.BaseDir)
+		rv[k] = pathtorel(v, c.BaseDir)
+	}
+	// fmt.Println("Env: ", rv)
+	return
+}
 
 func cmdhash(c config.Config, env map[string]string, cmd []string) (string, error) {
 	h := blake3.New(32, nil)
+	cmd = cmdnormalize(c, cmd)
 	for _, v := range cmd {
 		h.Write([]byte(v))
 	}
+	env = envnormalize(c, env)
 	tohashvars := map[string]string{}
 	for _, v := range c.Envars {
 		if v, exists := env[v]; exists {
--- whash/whash_test.go
+++ whash/whash_test.go
@@ -3,6 +3,7 @@ package whash
 import (
 	"config"
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -16,6 +17,76 @@ type TestDataPair struct {
 	d string
 	l TestData
 	r TestData
+}
+
+func TestCmdNormalize(t *testing.T) {
+	conf := config.Config{
+		BaseDir: "/a/b",
+	}
+	testCases := []struct {
+		d       string
+		basedir string
+		cmd     []string
+		ncmd    []string
+	}{
+		{
+			d:       "relative dirs",
+			basedir: "/a/b",
+			cmd:     []string{"g++", "-o", "file.o", "file.c"},
+			ncmd:    []string{"g++", "-o", "file.o", "file.c"},
+		},
+		{
+			d:       "absolute and relative dirs",
+			basedir: "/a/b",
+			cmd:     []string{"g++", "-o", "file.o", "/a/b/file.c"},
+			ncmd:    []string{"g++", "-o", "file.o", "./file.c"},
+		},
+	}
+	for _, tc := range testCases {
+		fmt.Println("\tSubTest: ", tc.d)
+		conf.BaseDir = tc.basedir
+		x := cmdnormalize(conf, tc.cmd)
+		if !reflect.DeepEqual(x, tc.ncmd) {
+			fmt.Println("Match Failed: ", tc.cmd, x, tc.ncmd)
+			t.Fail()
+		}
+
+	}
+}
+
+func TestEnvNormalize(t *testing.T) {
+	conf := config.Config{
+		BaseDir: "/a/b",
+	}
+	testCases := []struct {
+		d       string
+		basedir string
+		env     map[string]string
+		nenv    map[string]string
+	}{
+		{
+			d:       "absolute and relative dirs",
+			basedir: "/a/b",
+			env:     map[string]string{"CWD": "/a/b/c/d"},
+			nenv:    map[string]string{"CWD": "./c/d"},
+		},
+		{
+			d:       "relative and relative dirs",
+			basedir: "/a/b",
+			env:     map[string]string{"CWD": "d"},
+			nenv:    map[string]string{"CWD": "d"},
+		},
+	}
+	for _, tc := range testCases {
+		fmt.Println("\tSubTest: ", tc.d)
+		conf.BaseDir = tc.basedir
+		x := envnormalize(conf, tc.env)
+		if !reflect.DeepEqual(x, tc.nenv) {
+			fmt.Println("Match Failed: ", tc.env, x, tc.nenv)
+			t.Fail()
+		}
+
+	}
 }
 
 func TestHashMustMatch(t *testing.T) {
@@ -43,29 +114,29 @@ func TestHashMustMatch(t *testing.T) {
 				cmd: []string{"g++", "-o", "file.o", "file.c"},
 			},
 		},
-		// {
-		// 	d: "Same command and env, absolute vs relative path",
-		// 	l: TestData{
-		// 		conf: config.Config{
-		// 			BaseDir: "/a/b",
-		// 			Envars:  []string{"CWD"},
-		// 			Tools:   []config.Tool{},
-		// 			ToolIdx: -1,
-		// 		},
-		// 		env: map[string]string{"CWD": "/a/b/c"},
-		// 		cmd: []string{"g++", "-o", "file.o", "/a/b/c/file.c"},
-		// 	},
-		// 	r: TestData{
-		// 		conf: config.Config{
-		// 			BaseDir: "/a/b",
-		// 			Envars:  []string{"CWD"},
-		// 			Tools:   []config.Tool{},
-		// 			ToolIdx: -1,
-		// 		},
-		// 		env: map[string]string{"CWD": "/a/b/c"},
-		// 		cmd: []string{"g++", "-o", "file.o", "file.c"},
-		// 	},
-		// },
+		{
+			d: "Same command and env, absolute vs relative path args",
+			l: TestData{
+				conf: config.Config{
+					BaseDir: "/a/b",
+					Envars:  []string{"CWD"},
+					Tools:   []config.Tool{},
+					ToolIdx: -1,
+				},
+				env: map[string]string{"CWD": "/a/b/c"},
+				cmd: []string{"g++", "-o", "file.o", "/a/b/c/file.c"},
+			},
+			r: TestData{
+				conf: config.Config{
+					BaseDir: "/a/b",
+					Envars:  []string{"CWD"},
+					Tools:   []config.Tool{},
+					ToolIdx: -1,
+				},
+				env: map[string]string{"CWD": "/a/b/c"},
+				cmd: []string{"g++", "-o", "file.o", "./c/file.c"},
+			},
+		},
 	}
 	for _, tc := range testCases {
 		fmt.Println("\tSubTest: ", tc.d)
